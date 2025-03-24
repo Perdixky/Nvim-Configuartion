51e358043e3ca7db5172e8d69d38cdc3b4c45a73
Perdixky <3293789706@qq.com>
2025-03-24 16:58:00 +0800

    配置copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+  "AntoineGS/copilot.lua",
+  config = function()
+    require("copilot").setup({
+      copilot_model = "gpt-4o-copilot",
+    })
+  end,
+}
--- lua/plugins/termux-clangd.lua
+++ lua/plugins/termux-clangd.lua
@@ -1,23 +1,20 @@
--- require("config.lazy")
-local function is_termux()
-  if vim.fn.has("unix") == 1 then
-    if os.getenv("TERMUX_VERSION") ~= nil then
-      return true
-    end
-  end
-end
-
-if is_termux() then
-  return {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          clangd = {
-            mason = false
-          }
-        }
-      }
-    }
-  }
-end
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          mason = function()
+            if vim.fn.has("unix") == 1 then
+              if os.getenv("TERMUX_VERSION") ~= nil then
+                return false
+              else
+                return true
+              end
+            end
+          end,
+        },
+      },
+    },
+  },
+}
